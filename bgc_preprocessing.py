1b74ae614435697aa5de8a852e1f19e4c3828c37
Andrew Shen <andrew7shen@n0000.scs00>
2023-10-22 08:52:45 -0700

    Finished pseudocode

--- bgc_preprocessing.py
+++ bgc_preprocessing.py
@@ -3,25 +3,32 @@
 # Script to preprocess BGC genomes into chunks for input into species classification task
 
 
-# Read in the 10 BGC genomes
+# TODO: Read in the 10 BGC genomes
 curr_bgc_location = "/global/scratch/users/andrew7shen/data/species/"
 curr_human_location = "/global/scratch/users/andrew7shen/data/species_example"
 bgc_genome_paths = ["Bacillus_subtilis", "Enterococcus_faecalis", "Lactobacillus_fermentum", "Pseudomonas_aeruginosa", "Salmonella_enterica", "Cryptococcus_neoformans", "Escherichia_coli", "Listeria_monocytogenes", "Saccharomyces_cerevisiae", "Staphylococcus_aureus"]
 
-# Split them into the bacterial and yeast genomes
+# TODO: Read in human chr1 to see length and format
 
-# Iterate through each of the 7 training bacterial genomes
+# TODO: Split them into the bacterial and yeast genomes
 
-    # Store the entire genome as string curr_genome_str
+# TODO: Iterate through each of the 7 training bacterial genomes
+
+    # TODO: Store the entire genome as string curr_genome_str
+    # TODO: Check curr_genome_str length and create chart in notes of all lengths
     
-    # Iterate through curr_genome_str in blocks of 20k, skipping by 5k
+    # TODO: Initialize chr_num variable as 1
     
-        # Prepend string with info about chr # and species
+    # TODO: Iterate through curr_genome_str in blocks of 20k, skipping by 5k
     
-        # Save current block of 20k bp as curr_chunk_str
+        # TODO: Prepend string with info about chr # and species
+    
+        # TODO: Save current block of 20k bp as curr_chunk_str
         
-        # Match formatting to the human chromosome files
+        # TODO: Match formatting to the human chromosome files
         
-        # Write curr_chunk_str to appropriate directory as chr#.fa
+        # TODO: Write curr_chunk_str to appropriate directory as gzipped chr#.fa
+        
+        # TODO: Increment chr_num variable
         
         
